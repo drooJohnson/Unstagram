062b1472e2aae174758527b902bf83ecc47a9855
Patrick Lindsay <patrick.lindsay@me.com>
2018-05-02 11:11:44 -0500

    Set up unsplash.js for import

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,5 +20,11 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('node_modules/unsplash-js/dist/unsplash.min.js', {
+    using: [
+      { transformation: 'cjs', as: 'unsplash-js' }
+    ]
+  });
+
   return app.toTree();
 };
