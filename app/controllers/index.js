f7493af70de12b306a9d994bb617a546329db6b7
Patrick Lindsay <patrick.lindsay@me.com>
2018-05-02 11:17:36 -0500

    Set up the index controller and call the timeline component

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,27 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { get, set } from '@ember/object';
+
 export default Controller.extend({
+  likes: 0,
+  photographer: {},
+  photoUrl: 'http://placehold.it/200x200',
+
+  unsplash: service(),
+
+  init() {
+    this._super(...arguments);
+    this.send('getPhoto');
+  },
+
+  actions: {
+    async getPhoto() {
+      let json = await get(this, 'unsplash').getRandomPhoto();
+      let photoUrl = json.urls.regular;
+      let { likes, user } = json;
+      set(this, 'likes', likes);
+      set(this, 'photoUrl', photoUrl);
+      set(this, 'photographer', user);
+    }
+  }
 });
